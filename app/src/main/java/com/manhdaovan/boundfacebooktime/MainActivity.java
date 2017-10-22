b68acd1db788868944e8aea8a3b42de0a52569b1
Manh Dao Van <manhdaovan@gmail.com>
2017-10-22 22:06:56 +0900

    Change some texts

--- app/src/main/java/com/manhdaovan/boundfacebooktime/MainActivity.java
+++ app/src/main/java/com/manhdaovan/boundfacebooktime/MainActivity.java
@@ -79,9 +79,6 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         }
-//        else {
-//            super.onBackPressed();
-//        }
     }
 
     @Override
