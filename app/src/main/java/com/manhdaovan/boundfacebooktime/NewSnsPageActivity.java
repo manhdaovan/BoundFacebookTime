f99cc60cb56c0f806970effa356582c42e1c8481
Manh Dao Van <manhdaovan@gmail.com>
2017-11-08 22:23:57 +0900

    Add screenOrientation

--- app/src/main/java/com/manhdaovan/boundfacebooktime/NewSnsPageActivity.java
+++ app/src/main/java/com/manhdaovan/boundfacebooktime/NewSnsPageActivity.java
@@ -8,28 +8,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class NewSnsPageActivity extends AppCompatActivity {
     private FloatingActionButton fabAddNewSnsPage;
+    private EditText edtSnsName;
+    private EditText edtSnsUrl;
+    private Switch swUseAppTime;
+    private TextView tvSnsUseTime;
+    private SeekBar sbSnsUseTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null) {
-            actionBar.setTitle("vkl 123");
+            actionBar.setTitle("New webpage object");
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
         setContentView(R.layout.activity_new_sns_page);
-
-        fabAddNewSnsPage = (FloatingActionButton) findViewById(R.id.fab_add_new_sns);
-        fabAddNewSnsPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(NewSnsPageActivity.this, "save", Toast.LENGTH_SHORT).show();
-            }
-        });
+        setUp();
     }
 
     @Override
@@ -64,5 +67,52 @@ public class NewSnsPageActivity extends AppCompatActivity {
             }
         });
         return alertDialog;
+    }
+
+    private void setUp(){
+        fabAddNewSnsPage = (FloatingActionButton) findViewById(R.id.fab_add_new_sns);
+        fabAddNewSnsPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(NewSnsPageActivity.this, "save", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        edtSnsName = (EditText) findViewById(R.id.edt_sns_name);
+        edtSnsUrl = (EditText) findViewById(R.id.edt_sns_url);
+        tvSnsUseTime = (TextView) findViewById(R.id.tv_app_use_time);
+        sbSnsUseTime = (SeekBar) findViewById(R.id.sb_app_use_time);
+        swUseAppTime = (Switch) findViewById(R.id.sw_use_app_time);
+
+        swUseAppTime.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    sbSnsUseTime.setVisibility(View.GONE);
+                    tvSnsUseTime.setVisibility(View.GONE);
+                }
+                else {
+                    sbSnsUseTime.setVisibility(View.VISIBLE);
+                    tvSnsUseTime.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        sbSnsUseTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvSnsUseTime.setText(progress + " minute(s)");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
